9c010fbf68b06c53e2d5b28611ea2fcb537b3541
Chris Berg <chris.berg@aot-technologies.com>
2025-12-25 13:55:28 -0800

    fix: using sstore_namespace

--- modules/eso-secretstore/main.tf
+++ modules/eso-secretstore/main.tf
@@ -103,7 +103,7 @@ resource "helm_release" "external_secret_store_aws_irsa" {
                 jwt:
                   serviceAccountRef:
                     name: external-secrets
-                    namespace: "${var.eso_namespace}"
+                    namespace: "${var.sstore_namespace}"
     EOF
   ]
 }
